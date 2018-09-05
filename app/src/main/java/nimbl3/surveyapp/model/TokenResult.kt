7915e9156aa68a6532b5257a69d3a41f50f190df
Tobias Heuts <tobias@connectica.no>
2018-09-05 16:55:25 +0700

    viewpager init

--- app/src/main/java/nimbl3/surveyapp/model/TokenResult.kt
+++
@@ -1,3 +0,0 @@
-package nimbl3.surveyapp.model
-
-data class TokenResult(val access_token: String, val expires_in: Int)
--- app/src/main/java/nimbl3/surveyapp/service/SurveyApiService.kt
+++ app/src/main/java/nimbl3/surveyapp/service/SurveyApiService.kt
@@ -1,8 +1,8 @@
 package nimbl3.surveyapp.service
 
 import io.reactivex.Observable
-import nimbl3.surveyapp.model.SurveyResult
-import nimbl3.surveyapp.model.TokenResult
+import nimbl3.surveyapp.model.Survey
+import nimbl3.surveyapp.model.Token
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -14,10 +14,10 @@ import retrofit2.http.Query
 interface SurveyApiService {
 
     @GET("surveys.json")
-    fun getSurveys(@Header("Authorization") acces_token: String, @Query("page") page: Int, @Query("per_page") perPage: Int): Observable<List<SurveyResult>>
+    fun getSurveys(@Header("Authorization") acces_token: String, @Query("page") page: Int, @Query("per_page") perPage: Int): Observable<List<Survey>>
 
     @POST("oauth/token")
-    fun getToken(@Query("grant_type") grant_type: String, @Query("username") username: String, @Query("password") password: String) : Observable<TokenResult>
+    fun getToken(@Query("grant_type") grant_type: String, @Query("username") username: String, @Query("password") password: String) : Observable<Token>
 
     companion object Factory {
         val base_url = "https://nimbl3-survey-api.herokuapp.com/"
