73e1bf685a417a3235cdc552b35821862b70dc82
Tobias Heuts <tobias@connectica.no>
2018-09-06 01:45:42 +0700

    prepare toolbar

--- app/src/main/java/nimbl3/surveyapp/controller/SurveyIndexActivity.kt
+++ app/src/main/java/nimbl3/surveyapp/controller/SurveyIndexActivity.kt
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v4.view.ViewPager
+import android.support.v7.widget.Toolbar
 import android.util.Log
+import android.view.Menu
 import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
 import android.widget.ProgressBar
@@ -21,6 +23,7 @@ class SurveyIndexActivity : AppCompatActivity() {
     private lateinit var viewPager: ViewPager
     private lateinit var tabLayout: TabLayout
     private lateinit var progressBar : ProgressBar
+    private lateinit var toolbar : Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
@@ -50,13 +53,23 @@ class SurveyIndexActivity : AppCompatActivity() {
                 })
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
     private fun init() {
         apiService = SurveyApiService.create()
+
+        toolbar = findViewById(R.id.toolbar)
         viewPager = findViewById(R.id.view_pager)
         tabLayout = findViewById(R.id.tab_layout)
         tabLayout.setupWithViewPager(viewPager, true)
         progressBar = findViewById(R.id.progressBar)
         progressBar.visibility = VISIBLE
+
+        toolbar.setNavigationIcon(R.drawable.ic_refresh)
+        setSupportActionBar(toolbar)
     }
 
 
