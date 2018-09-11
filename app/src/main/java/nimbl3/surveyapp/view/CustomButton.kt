e223e9b64d413c3a07bdcaf34fcd01d32a488d96
Tobias Heuts <tobias@connectica.no>
2018-09-11 18:47:03 +0700

    custom fonts

---
+++ app/src/main/java/nimbl3/surveyapp/view/CustomButton.kt
@@ -0,0 +1,25 @@
+package nimbl3.surveyapp.view
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import android.widget.Button
+
+class CustomButton : Button {
+    constructor(context: Context) : super(context) {
+        setFont()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        setFont()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {
+        setFont()
+    }
+
+    private fun setFont() {
+        val font = Typeface.createFromAsset(context.assets, "fonts/Poppins-SemiBold.ttf")
+        setTypeface(font, Typeface.NORMAL)
+    }
+}
---
+++ app/src/main/java/nimbl3/surveyapp/view/CustomTextView.kt
@@ -0,0 +1,25 @@
+package nimbl3.surveyapp.view
+
+import android.content.Context
+import android.graphics.Typeface
+import android.widget.TextView
+import android.util.AttributeSet
+
+
+class CustomTextView : TextView {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        setFont(attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {
+        setFont(attrs)
+    }
+
+    private fun setFont(attrs: AttributeSet) {
+        val font = Typeface.createFromAsset(context.assets, "fonts/Poppins-Light.ttf")
+        setTypeface(font, Typeface.NORMAL)
+    }
+}
